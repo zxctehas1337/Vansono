1f203dc6809405dd6e5fa30b4842c09e6431ff82
nihadev <genii5831@gmail.com>
2025-10-17 22:30:30 +0400

    РђРІС‚РѕРјР°С‚РёС‡РµСЃРєРёР№ РєРѕРјРјРёС‚

--- a.bat
+++ a.bat
@@ -1,14 +1,38 @@
 @echo off
+setlocal enabledelayedexpansion
 color 2
-git add .
-echo "Please enter the commit message"
-set /p commit_message=
-git commit -m "%commit_message%"
-echo "Pushing to origin master?"
-set /p push_to_origin=
-if "%push_to_origin%" == "y" (
-    git push origin master
-) else (
-    echo "Pushing to origin master cancelled"
+chcp 1251 >nul
+
+:: Инициализация переменных
+set commit_message=Автоматический коммит
+set auto_push=false
+
+:: Обработка аргументов
+:parse_args
+if "%~1"=="" goto execute
+
+if "%~1"=="-y" (
+    set auto_push=true
+) else if "%~1"=="-m" (
+    shift
+    set commit_message=%~1
 )
+
+shift
+goto parse_args
+
+:execute
+echo Добавление файлов...
+git add .
+
+echo Коммит с сообщением: "%commit_message%"
+git commit -m "%commit_message%"
+
+if "%auto_push%"=="true" (
+    echo Пуш на origin master...
+    git push origin master
+)
+
 cmd /c cls
+chcp 65001 >nul
+endlocal
