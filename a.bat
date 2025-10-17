659480e0a25b91f467aa7ed5681bc7edcc09b493
nihadev <genii5831@gmail.com>
2025-10-17 17:30:07 +0400

    wgdbwdw

--- a.bat
+++ a.bat
@@ -4,4 +4,10 @@ git add .
 echo "Please enter the commit message"
 set /p commit_message=
 git commit -m "%commit_message%"
-git push origin master
+echo "Pushing to origin master?"
+set /p push_to_origin=
+if "%push_to_origin%" == "y" (
+    git push origin master
+) else (
+    echo "Pushing to origin master cancelled"
+)
